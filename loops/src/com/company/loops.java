c41c47813ce7787560bfe1ca0b3a8aebe9cf334d
m4dams <pataki.4d4m@gmail.com>
2021-10-18 15:29:05 +0200

    reformat & cleanup

--- loops/src/com/company/loops.java
+++ loops/src/com/company/loops.java
@@ -6,41 +6,40 @@ import java.util.Scanner;
 public class loops {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        Scanner scanner = new Scanner(System.in);
         //IWontCheatOnTheExams
-       /* for (int i = 0; i < 99; i++) {
+        /*for (int i = 0; i < 99; i++) {
             System.out.println("Nem fogok csalni az a vizsgán!");
-        }*/
+        }
 
         //PrintEven
-        /*for (int i = 0; i < 501; i+=2) {
+        for (int i = 0; i < 501; i += 2) {
             System.out.println(i);
         }*/
 
         //MultiplicationTable
-        /*Scanner scanner = new Scanner(System.in);
-        System.out.println("A program a megadott számhoz tartozó szorzótáblát írja ki.\n" +
+        /*System.out.println("A program a megadott számhoz tartozó szorzótáblát írja ki.\n" +
                 "Kérlek adj meg egy egész számot(lehet negatív is)");
-        int input=scanner.nextInt();
-        if (input==0){
+        int input = scanner.nextInt();
+        if (input == 0) {
             System.out.println("Minek akarod a 0-t szorozni? Mindig 0 marad. Nem pazarlom az időt hogy kiirjam neked");
         } else {
             for (int i = 1; i <= Math.abs(input); i++) {
-                System.out.println(i+" * "+input+" = "+(i*input));
+                System.out.println(i + " * " + input + " = " + (i * input));
             }
         }*/
 
         //CountFromTo
-        /*Scanner scanner = new Scanner(System.in);
-        System.out.println("A program két egész számot kér be SPACE-el elválasztva.\n" +
+        /*System.out.println("A program két egész számot kér be SPACE-el elválasztva.\n" +
                 "El fog számolni az első számtól a másodikig egyesével...\n" +
                 "Rajtad áll hány sort ír ki...\n" +
                 "Kérlek adj meg 2 db egész számot SPACE-el elválasztva");
         String input = scanner.nextLine();
-        String[] numbersIn=input.split(" ");
-        if (Integer.valueOf(numbersIn[0])==Integer.valueOf(numbersIn[1])) {
+        String[] numbersIn = input.split(" ");
+        if (Integer.valueOf(numbersIn[0]) == Integer.valueOf(numbersIn[1])) {
             System.out.println("Ez most vicc, igaz?");
-        } else if (Integer.valueOf(numbersIn[0])<Integer.valueOf(numbersIn[1])) {
+        } else if (Integer.valueOf(numbersIn[0]) < Integer.valueOf(numbersIn[1])) {
             for (int i = Integer.valueOf(numbersIn[0]); i <= Integer.valueOf(numbersIn[1]); i++) {
                 System.out.println(i);
             }
@@ -51,12 +50,12 @@ public class loops {
         }*/
 
         //FizzBuzz
-        /*for (int i = 0; i <101 ; i++) {
-            if ((i%3==0) && (i%5==0)){
+        /*for (int i = 0; i < 101; i++) {
+            if ((i % 3 == 0) && (i % 5 == 0)) {
                 System.out.println("FizzBuzz");
-            } else if (i%3==0){
+            } else if (i % 3 == 0) {
                 System.out.println("Fizz");
-            } else if (i%5==0) {
+            } else if (i % 5 == 0) {
                 System.out.println("Buzz");
             } else {
                 System.out.println(i);
@@ -64,24 +63,22 @@ public class loops {
         }*/
 
         //DrawTriangle
-        /*Scanner scanner = new Scanner(System.in);
-        System.out.println("A program bekér egy egész számot majd kirajzol egy háromszöget.\n" +
+        /*System.out.println("A program bekér egy egész számot majd kirajzol egy háromszöget.\n" +
                 "Kérlek adj meg egy egész számot (ne legyen túl nagy...)");
         int rows = scanner.nextInt();
-        for (int i = 1; i <=rows ; i++) {
-            for (int j = 1; j <=i ; j++) {
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }*/
 
         //DrawPyramid
-        /*Scanner scanner = new Scanner(System.in);
-        System.out.println("A program bekér egy egész számot majd kirajzol egy piramist.\n" +
+        /*System.out.println("A program bekér egy egész számot majd kirajzol egy piramist.\n" +
                 "Kérlek adj meg egy egész számot (ne legyen túl nagy...)");
         int rows = scanner.nextInt();
-        for (int i = 1; i <=rows ; i++) {
-            for (int j = rows-i; j > 0; j--) {
+        for (int i = 1; i <= rows; i++) {
+            for (int j = rows - i; j > 0; j--) {
                 System.out.print(" ");
             }
             for (int k = 1; k < i; k++) {
@@ -91,14 +88,13 @@ public class loops {
         }*/
 
         //DrawRhombus
-        /*Scanner scanner = new Scanner(System.in);
-        System.out.println("A program bekér egy egész számot majd kirajzol egy rombuszt.\n" +
+        /*System.out.println("A program bekér egy egész számot majd kirajzol egy rombuszt.\n" +
                 "Kérlek adj meg egy egész számot (ne legyen túl nagy...)");
         int rows = scanner.nextInt();
-        int lineHelper=(rows%2==0)? (rows/2):((rows/2)+1);
-        if (rows%2!=0){
-            for (int i = 1; i <=lineHelper ; i++) {
-                for (int j = lineHelper-i; j >= 0; j--) {
+        int lineHelper = (rows % 2 == 0) ? (rows / 2) : ((rows / 2) + 1);
+        if (rows % 2 != 0) {
+            for (int i = 1; i <= lineHelper; i++) {
+                for (int j = lineHelper - i; j >= 0; j--) {
                     System.out.print(" ");
                 }
                 for (int k = 1; k < i; k++) {
@@ -108,8 +104,8 @@ public class loops {
 
             }
         } else {
-            for (int i = 1; i <=lineHelper ; i++) {
-                for (int j = lineHelper-i; j > 0; j--) {
+            for (int i = 1; i <= lineHelper; i++) {
+                for (int j = lineHelper - i; j > 0; j--) {
                     System.out.print(" ");
                 }
                 for (int k = 1; k <= i; k++) {
@@ -119,8 +115,8 @@ public class loops {
             }
         }
 
-        for (int i = lineHelper; i >=1 ; i--) {
-            for (int j = lineHelper-i; j > 0; j--) {
+        for (int i = lineHelper; i >= 1; i--) {
+            for (int j = lineHelper - i; j > 0; j--) {
                 System.out.print(" ");
             }
             for (int k = i; k > 0; k--) {
@@ -131,19 +127,18 @@ public class loops {
         }*/
 
         //DrawSquare
-        /*Scanner scanner = new Scanner(System.in);
-        System.out.println("A program bekér egy egész számot majd kirajzol egy négyzetet.\n" +
+        /*System.out.println("A program bekér egy egész számot majd kirajzol egy négyzetet.\n" +
                 "Kérlek adj meg egy egész számot (ne legyen túl nagy...)");
         int rows = scanner.nextInt();
-        String fill="%";
-        for (int i = 1; i <=rows; i++) {
-            if (i==1 || i%rows==0) {
+        String fill = "%";
+        for (int i = 1; i <= rows; i++) {
+            if (i == 1 || i % rows == 0) {
                 for (int j = 0; j < rows; j++) {
-                  System.out.print("%");
+                    System.out.print("%");
                 }
             } else {
                 System.out.print("%");
-                for (int j = 0; j < (rows-2); j++) {
+                for (int j = 0; j < (rows - 2); j++) {
                     System.out.print(" ");
                 }
                 System.out.print("%");
@@ -152,19 +147,18 @@ public class loops {
         }*/
 
         //DrawSquareWithDiagonal
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("A program bekér egy egész számot majd kirajzol egy négyzetet átlóval.\n" +
+        /*System.out.println("A program bekér egy egész számot majd kirajzol egy négyzetet átlóval.\n" +
                 "Kérlek adj meg egy egész számot (ne legyen túl nagy...)");
         int rows = scanner.nextInt();
-        for (int i = 1; i <=rows; i++) {
-            if (i==1 || i%rows==0) {
+        for (int i = 1; i <= rows; i++) {
+            if (i == 1 || i % rows == 0) {
                 for (int j = 0; j < rows; j++) {
                     System.out.print("%");
                 }
             } else {
                 System.out.print("%");
-                for (int j = 0; j < (rows-2); j++) {
-                    if (j==(i-2)) {
+                for (int j = 0; j < (rows - 2); j++) {
+                    if (j == (i - 2)) {
                         System.out.print("%");
                     } else {
                         System.out.print(" ");
@@ -173,21 +167,21 @@ public class loops {
                 System.out.print("%");
             }
             System.out.println();
-        }
+        }*/
 
         //ParametricAverage
-        /*Scanner scanner = new Scanner(System.in);
+
         System.out.println("ez az izé bekér egy egész számot, majd utána bekér annyi számot,\n" +
                 " mint ammenyit megadtál elsőnek. Aztán kidobja ezeknek összegét és átlagát");
-        int cycle=scanner.nextInt();
-        ArrayList numbers= new ArrayList();
-        int sum=0;
+        int cycle = scanner.nextInt();
+        ArrayList numbers = new ArrayList();
+        int sum = 0;
         for (int i = 0; i < cycle; i++) {
             numbers.add(scanner.nextInt());
-            sum+=(int)numbers.get(i);
+            sum += (int) numbers.get(i);
         }
 
-        System.out.println("Összeg: "+ sum + "\tÁtlag: "+ (sum/ numbers.size()));
+        System.out.println("Összeg: " + sum + "\tÁtlag: " + (sum / numbers.size()));
 
         //DrawChessTable
         /*for (int i = 1; i < 9; i++) {
